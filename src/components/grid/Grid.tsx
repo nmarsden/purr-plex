de8f9007cd8fbd090c87d1aadca7bd354c3c682f
Neil Marsden <nmarsden@aconex.com>
2020-02-16 15:00:05 +1100

    size grid cells according to window dimensions

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import styles from './Grid.module.scss';
 
 function Grid() {
@@ -13,10 +13,20 @@ function Grid() {
     'OOOXXXOOO',
     'OOOXXXOOO'
   ];
+  const cellSize = useCellSize();
+  const cellStyles = {
+    width: `${cellSize}px`,
+    height: `${cellSize}px`
+  };
 
   const rows = cellTypes.map( (cellType, rowIndex) => {
     const cells: Array<ReactNode> = [];
-    cellType.split('').forEach( (ch, colIndex) =>  cells.push(<div key={colIndex} className={ch === 'O' ? styles.cell : styles.shadedCell}></div>));
+    cellType.split('').forEach( (ch, colIndex) =>  {
+      cells.push(<div key={colIndex}
+                      style={cellStyles}
+                      className={ch === 'O' ? styles.cell : styles.shadedCell}>
+                 </div>)
+    });
     return <div key={rowIndex} className={styles.row}>{cells}</div>;
   } );
 
@@ -25,6 +35,34 @@ function Grid() {
       {rows}
     </div>
   );
+}
+
+function getWindowDimensions() {
+  const { clientWidth: width, clientHeight: height } = window.document.documentElement;
+  return {
+    width,
+    height
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
+function useCellSize() {
+  const { height, width } = useWindowDimensions();
+  return Math.floor( Math.min( height / 9, width / 9) );
 }
 
 export default Grid;
